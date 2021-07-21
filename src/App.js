d8ca91a1b4b4ef8d81c40e05c999ff27012510d6
tuomax7 <tuomas.nummela.per@gmail.com>
2021-07-21 21:27:26 +0300

    Prototype of leaderboard by seasons now complete.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Leaderboard from './components/Leaderboard.js'
 
 function App() {
   return (
     <div>
       <h1>GolfTour</h1>
+      <Leaderboard />
     </div>
   );
 }
---
+++ src/components/Leaderboard.js
@@ -0,0 +1,79 @@
+import { select } from 'async'
+import React from 'react'
+import {useState} from 'react'
+
+
+
+const Leaderboard = () => {
+
+    const [selectedSeason, setSelectedSeason] = useState(2021)
+
+    
+    const SeasonSelector = () => {
+    
+        //When selected season changes, selected seasonstate updates
+        const selectedSeasonChange = () => setSelectedSeason(document.getElementById('seasonSelector').value)
+    
+        const seasons = [2021, 2022, 2023]
+        return(
+            <div>
+                Valitse kisakausi:
+                <select id="seasonSelector" onChange={selectedSeasonChange} defaultValue={selectedSeason}>
+                    {seasons.map(season => <option key={season}>{season}</option>)}
+                </select>
+            </div>
+        )
+    }
+
+
+
+    const BoardListing = () => {
+
+        const leaderboard = [
+            {
+                name: "Johannes Sippola",
+                score: [300, 200, 100]
+            },
+            {
+                name: "Joel Vanhanen",
+                score: [100, 200, 300]
+            },
+            {
+                name: "Tuomas Nummela",
+                score: [400, 200, 100]
+            }
+        ]
+
+        //Custom season- and scorebased sorting for leaderboard
+    
+        const sortedLeaderboard = leaderboard.sort((a, b) => {
+            if(a.score[selectedSeason-2021] > b.score[selectedSeason-2021]){
+                return -1
+            }
+            else return 1
+        })
+    
+        return(
+            <div>
+                <ol>
+                    {sortedLeaderboard.map(contestant => 
+                        <li key={contestant.name}>{contestant.name} {contestant.score[selectedSeason-2021]}</li>
+                    )}
+                </ol>
+            </div>
+        )
+    }
+
+
+
+    return(
+        <div>
+            <SeasonSelector />
+            <BoardListing />
+        </div>
+    )
+}
+
+
+
+export default Leaderboard
