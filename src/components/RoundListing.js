2263f2822e294d2d6096ee5b8d0a708012cc3211
tuomax7 <tuomas.nummela.per@gmail.com>
2021-07-27 18:55:55 +0300

    Roundlisting now sorts rounds by date.

--- src/components/RoundListing.js
+++ src/components/RoundListing.js
@@ -1,6 +1,22 @@
 import React from 'react'
 
 const RoundListing = ({currentSeason, contestants, rounds}) => {
+
+    const sortedSeasonRounds = [...rounds[currentSeason-2021]]
+    sortedSeasonRounds.sort((a, b) => {
+        const dateArrayA = a.date.split('.').map(itemA => parseInt(itemA))
+        const dateArrayB = b.date.split('.').map(itemB => parseInt(itemB))
+
+        if(dateArrayA[2] === dateArrayB[2]){
+
+            if(dateArrayA[1] === dateArrayB[1]){
+
+                return dateArrayB[0] - dateArrayA[0]
+            }
+            return dateArrayB[1] - dateArrayA[1]
+        }
+        return dateArrayB[2] - dateArrayA[2]
+    })
     
     return(
         <div>
@@ -13,7 +29,7 @@ const RoundListing = ({currentSeason, contestants, rounds}) => {
                         {contestants.map(contestant => <th key={contestant.name}>{contestant.name}</th>)}
                     </tr>
                     
-                    {rounds[currentSeason-2021].map(round => 
+                    {sortedSeasonRounds.map(round => 
                         <tr key={round.date+round.johannes+round.joel+round.tuomas}>
                             <td>{round.date}</td>
                             <td>{round.course}</td>
